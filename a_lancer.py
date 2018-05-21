42c4ac55a5e601b1f06691058d22f2c1736d0262
Arthur <arthur.lacoin@gmail.com>
2018-05-21 12:01:19 +0200

    fjdksqfq

--- a_lancer.py
+++ a_lancer.py
@@ -1,20 +1,22 @@
+from classes import *
+from tipe_en_cours import *
 
+GEO2 = [[3 for i in range(50)] for j in range(50)]
 
-GEO2 = [[3 for i in range(300)] for j in range(300)]
-
-ville = cases_touchees(72, 198, 2, 0, 300, 0, 300)
+ville = cases_touchees(31, 45, 2, 0, 50, 0, 50)
 
 for i, j in ville:
     GEO2[i][j] = 90
 
-g = Grille(300, GEO2, 10000000)
-g.cellules[150][150].repartition[1] += 30000
-g.cellules[150][150].population += 30000
+g = Grille(50, GEO2, 100000)
+g.cellules[25][25].repartition[1] += 300
+g.cellules[25][25].population += 300
 
-virus = [0.6, 0.5, 0.3]
+virus = [0.9, 0.05, 0.05]
 
 statistiques_2(g, virus)
 
+"""
 ville = cases_touchees(150, 150, 1, 0, 300, 0, 300)
 for i, j in ville:
     GEO2[i][j] = 90
@@ -26,3 +28,4 @@ g2.cellules[150][150].population += 30000
 virus = [0.6, 0.5, 0.3]
 
 statistiques_2(g2, virus)
+"""
--- classes.py
+++ classes.py
@@ -1,4 +1,5 @@
 import random
+import copy
 
 # Repartition est [Sain, Malade, Mort, Gueri]
 # Population est malade + sain + gueri
@@ -21,26 +22,33 @@ class Cellule:
     def repartition_pr_vivant(self):
         repartition_pourcentage = []
         for i in self.repartition:
-            if (self.repartition[0] + self.repartition[1] + self.repartition[3])==0:
+            if self.population == 0:
                 repartition_pourcentage.append(0)
             else :
-                repartition_pourcentage.append(i / (self.repartition[0] + self.repartition[1] + self.repartition[3]))
+                repartition_pourcentage.append(i / self.population)
 
         return repartition_pourcentage
 
     # virus = [proba_infection, proba_mort, proba_soin]
     def changement_interne(self, virus):
-        ancien_etat = self.repartition.copy()
+        self.population = self.repartition[0] + self.repartition[1] + self.repartition[3]
+        ancien_etat = copy.copy(self.repartition)
+        # On calcule les infectés
         if self.population == 0:
             self.repartition[1] = 0
+            self.repartition[0] = 0
+            self.repartition[3] = 0
+            print("Pas très normal que ça s'affiche.")
+            return
         else :
-            self.repartition[1] += (ancien_etat[0] * virus[0] * self.repartition[1] / self.population)
-        self.repartition[0] = ancien_etat[0] +  ancien_etat[1] - self.repartition[1]
+            self.repartition[1] += int(ancien_etat[0] * virus[0])
+            self.repartition[0] = ancien_etat[0] +  ancien_etat[1] - self.repartition[1]
 
         # On calcule les morts avant les gueris car, je cite "mourir c'est plus rapide que guerir"
 
         self.repartition[2] += int(self.repartition[1] * virus[1])
         self.repartition[1] -= int(self.repartition[1] * virus[1])
+
         self.repartition[3] += int(self.repartition[1] * virus[2])
         self.repartition[1] -= int(self.repartition[1] * virus[2])
         
@@ -100,32 +108,34 @@ class Grille:
                     proba_mvt = 0.1
                 elif(geographie[i][j] == route):
                     attractivite = 0.7
-                    proba_mvt = 1
+                    proba_mvt = 0.7
                 elif(geographie[i][j] == montagne):
                     attractivite = 0.1
                     proba_mvt = 0.1
 
                 # On ajoute une part d'aléatoire dans la répartition de la population
 
-                sous_liste.append(
-                    Cellule(int(population_totale * geographie[i][j] / total_geo * random.uniform(0.98, 1.02)), proba_mvt, attractivite))
+                sous_liste.append(Cellule(int(population_totale * geographie[i][j] / total_geo * random.uniform(0.98, 1.02)), proba_mvt, attractivite))
 
             self.cellules.append(sous_liste)
 
     def next(self, virus):
-        nouvelle_cellules = self.cellules.copy()
+        nouvelle_cellules = copy.deepcopy(self.cellules)
+        print(grille_pop_sum(self))
         for i in range(len(self.cellules)):
             for j in range(len(self.cellules[i])):
                 # Calcul de la population partie
-                population_partie = self.cellules[i][j].population * self.cellules[i][j].prob_mvt
-                nouvelle_cellules[i][j].population -= population_partie
 
                 repartitions = self.cellules[i][j].repartition_pr_vivant()
+                population_partie = int(self.cellules[i][j].population * self.cellules[i][j].prob_mvt)
+                nouvelle_cellules[i][j].population -= population_partie
 
                 # On répartit les pertes de population
                 nouvelle_cellules[i][j].repartition[0] = int(nouvelle_cellules[i][j].population * repartitions[0])
                 nouvelle_cellules[i][j].repartition[1] = int(nouvelle_cellules[i][j].population * repartitions[1])
                 nouvelle_cellules[i][j].repartition[3] = int(nouvelle_cellules[i][j].population * repartitions[3])
+
+
                 # Choix de la case où la population part
                 voisins = cases_touchees(i, j, 1, 0, self.taille, 0, self.taille)
 
@@ -133,16 +143,21 @@ class Grille:
                 nouvelle_liste_probas = []
                 
                 for k in range(len(liste_probas)):
-                    nouvelle_liste_probas.append( liste_probas[k]/sum(liste_probas))
+                    nouvelle_liste_probas.append(liste_probas[k]/sum(liste_probas))
 
                 x, y = voisins[choix_proba(nouvelle_liste_probas)]
                 nouvelle_cellules[x][y].population += population_partie
-                nouvelle_cellules[x][y].repartition[0] += int(population_partie * self.cellules[i][j].repartition_pr_vivant()[0])
-                nouvelle_cellules[x][y].repartition[1] += int(population_partie * self.cellules[i][j].repartition_pr_vivant()[1])
-                nouvelle_cellules[x][y].repartition[1] += int(population_partie * self.cellules[i][j].repartition_pr_vivant()[3])
-                nouvelle_cellules[i][j].changement_interne(virus)
-
+                nouvelle_cellules[x][y].repartition[0] += int(population_partie * repartitions[0])
+                nouvelle_cellules[x][y].repartition[1] += int(population_partie * repartitions[1])
+                nouvelle_cellules[x][y].repartition[3] += int(population_partie * repartitions[3])
+        
         self.cellules = nouvelle_cellules
+
+        for i in range(len(self.cellules)):
+            for j in range(len(self.cellules)):
+                self.cellules[i][j].changement_interne(virus)
+
+        print(grille_pop_sum(self))
 
     def stats(self):
         valeur_de_retour = [0, 0, 0, 0]
@@ -162,3 +177,11 @@ def grille_pop(g):
     return L
 
 
+def grille_pop_sum(g):
+    somme = 0
+    for i in range(len(g.cellules)):
+        for j in range(len(g.cellules)):
+            somme += g.cellules[i][j].population
+    
+    return somme
+        
